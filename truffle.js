3055311c007f2eb9e828e2acd1f02a44d4b53cbf
Clement Lakhal <clementlakhal@gmail.com>
2019-12-19 20:20:04 +0100

    :wrench: Update truffle.js

--- truffle.js
+++ truffle.js
@@ -11,7 +11,7 @@ module.exports = {
       host: '127.0.0.1',
       port: 8545,
       network_id: '*',
-      gas: 4000000,
+      gas: 10000000,
       gasPrice: Utils.toWei('2', 'gwei'),
     },
     rinkeby: {
