daa25a6a0a815957c9e048f72573816e5b2cecce
Clement Lakhal <clementlakhal@gmail.com>
2019-12-21 04:22:11 +0100

    :wrench: Update prize delay on Rinkeby

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,7 +5,7 @@ const SecretSanta = artifacts.require('SecretSanta');
 const SantaNonFungibleToken = artifacts.require('SantaNonFungibleToken');
 
 function deployContracts(deployer, network) {
-  const prizeDelay = network === 'live' ? 60 * 60 * 24 * 3 : 60 * 30;
+  const prizeDelay = network === 'live' ? 60 * 60 * 24 * 3 : 60 * 60 * 3;
 
   deployer.deploy(SecretSanta, prizeDelay);
 
